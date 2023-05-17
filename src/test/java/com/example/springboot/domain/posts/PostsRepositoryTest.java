2d7f16ab1cb9b708e63afd2b54da77ff99178241
youngseok.jang <ys123.jang@gmail.com>
2023-05-17 09:09:25 +0900

    add posts CRUD

---
+++ src/test/java/com/example/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.example.springboot.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void load_post() {
+        // given
+        String title = "test title";
+        String content = "test content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("test@gmail.com")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
