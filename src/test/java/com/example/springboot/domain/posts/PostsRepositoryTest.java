ef66a6b2266cca88dfb6ea49cfe9d557b35dd507
youngseok.jang <ys123.jang@gmail.com>
2023-05-17 16:10:16 +0900

    add datetime

--- src/test/java/com/example/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/example/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.springboot.domain.posts;
 
+import org.apache.tomcat.jni.Local;
 import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,6 +45,29 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void test_BaseTimeEntity() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+
+        System.out.println("posts.getCreatedTime() = " + posts.getCreatedTime());
+        System.out.println("posts.getModifiedDate() = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedTime()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }
