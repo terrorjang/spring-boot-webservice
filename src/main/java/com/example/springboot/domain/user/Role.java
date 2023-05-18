37b22954e85ffe3e42e2b7bc87eb5c8c1a1da7b0
youngseok.jang <ys123.jang@gmail.com>
2023-05-18 17:28:32 +0900

    add google OAuth2

---
+++ src/main/java/com/example/springboot/domain/user/Role.java
@@ -0,0 +1,13 @@
+package com.example.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    GUEST("ROLE_GUEST", "GUEST"),
+    USER("ROLE_USER", "USER");
+    private final String key;
+    private final String title;
+}
